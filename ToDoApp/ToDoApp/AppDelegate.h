24f46ab94100db57732c61c09e36f9ecaa52eef3
Nguyen Ha Giang <giangnth@nitrotech.vn>
2014-12-12 16:56:29 +0700

    update
    update

---
+++ ToDoApp/ToDoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by NGUYENHAGIANG on 12/12/14.
+//  Copyright (c) 2014 NGUYENHAGIANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
